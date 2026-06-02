de33955570e3b96750085be3c2bf02a70885e221
John Ray <johnraycaete@gmail.com>
2026-06-02 11:58:34 +0800

    add cors option

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import { errorMiddleware } from "./middlewares/error.middleware";
 import authRoutes from "./modules/auth/index";
 import eventRoutes from "./modules/events/index"
 import path from "path";
 const app = express();
 
-app.use(cors());
+const corsOptions: CorsOptions = {
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+};
+
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/public", express.static(path.join(process.cwd(), "public")));
 
